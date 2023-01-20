9666d409a64a2f325103e7911a84ed6ae259d327
Irina77777777 <drdpi@ukr.net>
2023-01-20 22:52:39 +0200

    don't work router

---
+++ src/container/Task1/Task1.tsx
@@ -0,0 +1,55 @@
+import { Container, TextField, Typography } from '@mui/material'
+import { useState } from 'react'
+
+type Props = {}
+const Task1 = (props: Props) => {
+ const [count, setCount] = useState<number>(0)
+ const [count2, setCount2] = useState<number>(0)
+ const [count1, setCount1] = useState<number>(0)
+    return (
+        <div>
+            <Typography
+                variant="h3"
+                align="center"
+                component="h1"
+                color="blueviolet"
+                sx={{ padding: '50px 0' }}
+            >
+                First task :
+            </Typography>
+            <Typography
+                variant="h5"
+                align="center"
+                component="h1"
+                color="blueviolet"
+                sx={{ padding: '50px 0' }}
+            >
+                Kovalenko Iryna FE2609
+            </Typography>
+
+            <Container maxWidth="lg" sx={{ padding: '50px 10px' }}>
+                <div className="buttonchiki">
+                    <TextField
+                        label="Change Count 1"
+                        value={count}
+                        variant="outlined"
+                        onClick={() => setCount(count + 1)}
+                    ></TextField>
+                    <TextField
+                        label="Change Count 2"
+                        value={count2}
+                        variant="outlined"
+                        onClick={() => setCount2(count2 + 1)}
+                    ></TextField>
+                    <TextField
+                        label="Change Count 3"
+                        value={count1}
+                        variant="outlined"
+                        onClick={() => setCount1(count1 + 1)}
+                    ></TextField>
+                </div>{' '}
+            </Container>
+        </div>
+    )
+}
+export default Task1
---
+++ src/container/Task2/Task2.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+const Task2 = (props: Props) => {
+  return (
+    <div>Task2</div>
+  )
+}
+export default Task2
