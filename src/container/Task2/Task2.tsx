8230c6584507a2b6f0f7df7ee39c2e3e24f75852
Irina77777777 <drdpi@ukr.net>
2023-02-01 22:07:23 +0200

    task2 with coefficient

--- src/container/Task2/Task2.tsx
+++ src/container/Task2/Task2.tsx
@@ -17,6 +17,7 @@ type ValData = {
      totalPrice: number
 }
 
+
 const Task2 = (price: Props
 ) => {
     const [totalData, setTotalData] = useState<TotalData>({
@@ -24,33 +25,55 @@ const Task2 = (price: Props
         totalPrice: 0,
     })
 
+
     const [valData, setValData] = useState<ValData>({
     oneiP: 750,
     twoiP:  850,
     threeiP: 1250,
         val: 'EUR',
-     totalPrice: 0,
+        totalPrice: 1
     })
+
+    const uanPriceToTotal = (
+        one: number,
+        two: number,
+        three: number,
+        va: string,
+        totalPrice: number, count: number
+    ) => {
+        setValData((prevStat: ValData) => ({
+            ...prevStat,
+
+            //one: prevStat.oneiP === 2250 ? (count = 10) : (count = 10),
+            // two: prevStat.twoiP === 2000 ? (count = 2) : (count = 2),
+            // three: prevStat.threeiP === 50000 ? (count = 40) : (count = 40),
+            oneiP: 750* count,
+            twoiP: 850* count,
+            threeiP: 1250* count,
+            val: va,
+            totalPrice: prevStat.totalPrice * count,
+        }))
+    }
+       
+         
+      
 
     const addPriceToTotal = (count: number, price: number) => {
         setTotalData((prevState: TotalData) => ({
-
             totalCount: prevState.totalCount + count,
             totalPrice: prevState.totalPrice + price * count,
-
         }))
     }
 
-    const uanPriceToTotal = (one: number, two: number, three: number,
-        va: string, totalPrice: number) => {
-        setValData({    
-            oneiP: one,
-            twoiP: two,
-            threeiP: three,
-            val: va, 
-             totalPrice: 0
-}
-       ) }
+//     const uanPriceToTotal = (one: number, two: number, three: number,
+//         va: string, totalPrice: number) => {
+//         setValData({    
+//             oneiP: one,
+//             twoiP: two,
+//             threeiP: three,
+//             val: va, 
+//              totalPrice: 0
+// } ) }
 
     return (
         <>
@@ -73,7 +96,7 @@ const Task2 = (price: Props
                             40000,
                             50000,
                             'UAN',
-                            (totalData.totalPrice = totalData.totalPrice * 18)
+                            (totalData.totalPrice = totalData.totalPrice * 18), 40
                         )
                     }
                 >
@@ -89,7 +112,7 @@ const Task2 = (price: Props
                             'EUR',
                             (totalData.totalPrice = Math.round(
                                 totalData.totalPrice * 0.025
-                            ))
+                            )), 1
                         )
                     }
                 >
@@ -104,8 +127,8 @@ const Task2 = (price: Props
                             3000,
                             'USD',
                             (totalData.totalPrice = Math.round(
-                                totalData.totalPrice * 1.24
-                            ))
+                                totalData.totalPrice * 1.35
+                            )), 1.5
                         )
                     }
                 >
@@ -115,12 +138,13 @@ const Task2 = (price: Props
                     variant="outlined"
                     onClick={() =>
                         uanPriceToTotal(
-                            3000,
-                            4000,
-                            5000,
+                            2250,
+                            2550,
+                            3750,
                             'AED',
-                            (totalData.totalPrice =
-                                Math.round(totalData.totalPrice * 1.79))
+                             (totalData.totalPrice =
+                            Math.round(totalData.totalPrice * 1.75)),
+                            3
                         )
                     }
                 >
